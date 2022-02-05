f3934ac8cd38642b99117912c2888178971f3c00
mahdi <marjani.mahdi@gmail.com>
2022-02-05 18:50:00 +0330

    update

--- apps/students/admin.py
+++ apps/students/admin.py
@@ -5,9 +5,10 @@ from django.db.models import Count
 from .models import students
 from apps.parents.models import parents
 
+
 @admin.register(students)
 class studentsAdmin(admin.ModelAdmin):
-    list_display = ("name", "family","parent","parent_age")
+    list_display = ("name", "family", "parent", "parent_age")
 
     @admin.display()
     def parent_age(self, obj):
@@ -15,12 +16,11 @@ class studentsAdmin(admin.ModelAdmin):
 
     def changelist_view(self, request, extra_context=None):
         extra_context = extra_context or {}
-        extra_context['greate60'] = parents.objects.filter(age__gte=60).aggregate(all=Count("id"))
-        
+        extra_context['greate60'] = parents.objects.filter(
+            age__gte=60).aggregate(all=Count("id"))
+
         response = requests.get(
             'http://127.0.0.1:8000/api/v1/students/report/50')
         extra_context['greate50'] = len(response.json())
 
-
         return super(studentsAdmin, self).changelist_view(request, extra_context=extra_context)
-
